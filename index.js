d9387e3ef1e3a56788fb98f39de873f9ccb94195
RamonPloP <94577562+RamonPloP@users.noreply.github.com>
2023-09-20 13:12:54 -0600

    Correción index.js

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/results/:n1/:n2', (req,res)=> {
     res.send('Suma = '+(parseInt(req.params.n1,10)+parseInt(req.params.n2,10)));
 });
 
-app.post('/results/:n1/:n2', (req,res)=>{
-    res.send('Multiplicacion = '+(parseInt(req.params.n1,10)*parseInt(req.params.n2,10)));
+app.post('/results/', (req,res)=>{
+    res.send('Multiplicacion = '+(parseInt(req.body.n1,10)*parseInt(req.body.n2,10)));
 });
 
-app.put('/results/:n1/:n2', (req,res)=>{
-    res.send('Division = '+(parseInt(req.params.n1,10)/parseInt(req.params.n2,10)));
+app.put('/results/', (req,res)=>{
+    res.send('Division = '+(parseInt(req.body.n1,10)/parseInt(req.body.n2,10)));
 });
 
-app.patch('/results/:n1/:n2', (req,res)=>{
-    res.send('Potencia = '+(Math.pow(parseInt(req.params.n1,10),parseInt(req.params.n2,10))));
+app.patch('/results/', (req,res)=>{
+    res.send('Potencia = '+(Math.pow(parseInt(req.body.n1,10),parseInt(req.body.n2,10))));
 });
 
 app.delete('/results/:n1/:n2', (req,res)=>{
